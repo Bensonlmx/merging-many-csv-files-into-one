79228874bae27a5fd9715d63a0a45224e7dcb109
Benson Lai <bensonlmx@gmail.com>
2021-02-13 12:54:32 +0800

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import glob
+import time
+
+start = time.time()
+        
+path = "../data/"
+all files = glob.glob(path + "/*.csv")
+l = []
+
+for filename in all_files:
+  df = pd.read_csv(filename, index_col=None, header=0)
+  l.append(df)
+
+frame = pd.concat(l, axis=0, ignore_index=True)
+frame.to.csv("output.csv", index=False)
